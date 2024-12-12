3a3edf4ee9e1596c82a80e416d69ba3f62a1c2d4
dpvasani <vadanidarshan@gmail.com>
2024-12-12 23:25:07 +0530

    Ts Ch 1

--- Ch1.ts
+++ Ch1.ts
@@ -1,1 +1,10 @@
 var a =15;
+var b = 5;
+var sum = a+b;
+console.log(sum);
+
+// We need to compile the file using the following command
+// tsc filename.ts
+function add(a:number,b:number):number{
+    return a+b;
+}
