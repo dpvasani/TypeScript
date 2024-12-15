6edc4ceb153051d42619cd4b498ad539c6f5b2b6
dpvasani <vadanidarshan@gmail.com>
2024-12-16 01:00:57 +0530

    Object, Array, Tuple, Enum

--- Lecture 2/Ch2.js
+++ Lecture 2/Ch2.js
@@ -1,92 +1,59 @@
 // Object, Array, Tuple, Enum
 //-------------------- Object --------------------//
-
 // Defining an object with nested properties
-const person: {
-    firstName: string;
-    age: number;
-    address: {
-        city: string;
-    };
-} = {
+var person = {
     firstName: "Patel",
     age: 21,
     address: {
         city: "Mumbai",
     },
 };
-
 //-------------------- Array --------------------//
-
 // Object with array property
-const personWithSkills: {
-    firstName: string;
-    age: number;
-    skills: string[];
-} = {
+var personWithSkills = {
     firstName: "Patel",
     age: 21,
     skills: ["ReactJS", "NodeJS"],
 };
-
 // Simple string array
-let favouriteLanguages1: string[];
+var favouriteLanguages1;
 favouriteLanguages1 = ["Hindi", "English"];
-
 // Array with flexible types using `any[]`
-let favouriteLanguages2: any[];
+var favouriteLanguages2;
 favouriteLanguages2 = ["Hindi", "English", 21, true];
-
 //-------------------- Tuple --------------------//
-
 // Object with a tuple
-const personWithTuple: {
-    name: string;
-    age: number;
-    skills: string[];
-    product: [number, string]; // Fixed array: first element -> number, second -> string
-    // product: [number, string, string]; // Invalid: Tuple length mismatch
-    // product: [number, number]; // Invalid: Type mismatch
-    // Fixed Array Of Type
-} = {
+var personWithTuple = {
     name: "Patel",
     age: 21,
     skills: ["React", "Node"],
     product: [10, "Macbook M2"],
 };
-
 // personWithTuple.product = [10, "Macbook M2", "ExtraValue"]; // Invalid: Tuple length mismatch
 // personWithTuple.product[1] = 20; // Invalid: Type mismatch
-
 //-------------------- Enum --------------------//
-
 // Enum declaration for roles
-enum Role {
-    ADMIN,
-    AUTHOR,
-    READ_USER_ONLY,
-}
-
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["AUTHOR"] = 1] = "AUTHOR";
+    Role[Role["READ_USER_ONLY"] = 2] = "READ_USER_ONLY";
+})(Role || (Role = {}));
 // Object using the `Role` enum
-const user: {
-    name: string;
-    age: number;
-    skills: string[];
-    product: [number, string];
-    role: Role;
-} = {
+var user = {
     name: "Patel",
     age: 21,
     skills: ["React", "Node"],
     product: [10, "Macbook Air M2"],
     role: Role.READ_USER_ONLY,
 };
-
 // Role-based checks
 if (user.role === Role.AUTHOR) {
     console.log("User is an Author");
-} else if (user.role === Role.ADMIN) {
+}
+else if (user.role === Role.ADMIN) {
     console.log("User is an Admin");
-} else if (user.role === Role.READ_USER_ONLY) {
+}
+else if (user.role === Role.READ_USER_ONLY) {
     console.log("User has read-only access");
 }
